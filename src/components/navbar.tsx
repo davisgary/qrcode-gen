3340f3dfa9be36e80ac8474be358a83b3b1102e6
Gary Davis <gary.davis@fifco.com>
2023-10-31 13:02:02 -0400

    Update navbar and add logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Logo from '../images/logo.svg';
+
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-transparent p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-transparent p-6 lg:px-24">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link to="/">
-          <img className="h-8 w-8" src="path_to_logo.png" alt="Logo" />
+          <img className="h-8 w-8" src={Logo} alt="Logo" />
         </Link>
       </div>
-      <div className="text-sm flex-grow text-right">
-        <Link to="/" className="block mt-4 inline-block text-black hover:text-gray-400 mr-4">
-          Home
-        </Link>
-        <Link to="/about" className="block mt-4 inline-block text-black hover:text-gray-400 mr-4">
+      <div className="text-sm flex-grow flex items-center justify-end">
+        <Link
+          to="/"
+          className="inline-block flex items-center mr-8 text-base font-sans font-medium text-black hover:text-gray-400"
+        >
           About
         </Link>
-        <Link to="/contact" className="block mt-4 inline-block text-black hover:text-gray-400">
-          Contact
+        <Link to="/about" className="inline-block flex items-center">
+          <button className="px-6 py-2 text-base font-sans font-medium text-white bg-black hover:bg-gray-800 rounded-md">
+            Create Your QR Code
+          </button>
         </Link>
       </div>
     </nav>
