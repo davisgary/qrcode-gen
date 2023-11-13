5ac634f6304b96791bb9de92c97a4a7efc5c4962
fifcogdavis <gary.davis@fifco.com>
2023-11-12 20:27:22 -0500

    Add grid and max width to container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "gatsby"; // Import Link from Gatsby
+import { Link } from "gatsby";
 import { HeadFC, PageProps } from "gatsby";
 import Layout from "../components/layout";
 import '../styles/global.css';
@@ -7,11 +7,11 @@ import '../styles/global.css';
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <main className="container mx-auto my-8 mt-32">
-        <h1 className="text-6xl text-center font-sans font-bold">Need A QR Code Fast?</h1>
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 my-8 mt-32 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-5xl">
+        <h1 className="text-6xl text-center font-sans font-bold">Need A Quick QR Code?</h1>
         <p className="text-lg text-center text-xl mt-6">Generate a QR Code for your URL in seconds, using this quick and easy generator.</p>
         <div className="text-center mt-8">
-          <Link to="/start" className="px-6 py-3 text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <Link to="/start" className="px-6 py-3 text-white text-base font-sans font-medium bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
             Get Started Generating Your QR Code
           </Link>
         </div>
--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -26,21 +26,21 @@ const StartPage: React.FC = () => {
 
   return (
     <Layout>
-      <div className="container mx-auto my-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 my-8 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl">
         <h1 className="text-5xl font-bold text-center">Get Started</h1>
         <p className="text-lg text-center mt-4">Generate a QR Code for your URL in seconds</p>
 
-        <div className="my-8 text-center">
+        <div className="my-8 text-center grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2">
           <input
             type="url"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             placeholder="Enter URL here"
-            className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-blue-600"
+            className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-blue-600 w-full"
           />
           <button
             onClick={generateQRCode}
-            className="ml-4 px-6 py-2 text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700"
+            className="px-6 py-2 text-white text-base font-sans font-medium bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 w-full md:w-auto"
           >
             Generate QR Code
           </button>
